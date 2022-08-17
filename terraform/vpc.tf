6c450dba6fa78966d13ce1c08e058d72b22769c3
bhagwatsapkota <sapkotabhagwat@gmail.com>
2022-08-17 13:08:20 -0400

    update

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -9,12 +9,15 @@ resource "aws_vpc" "final-vpc" {
     }
 }
 
+data "aws_availability_zones" "available" {}
+
+
 #Create two subnets
 resource "aws_subnet" "final-subnet-1" {
     vpc_id = aws_vpc.final-vpc.id
     cidr_block = "10.0.1.0/24" 
     map_public_ip_on_launch = true
-    availability_zone = "us-east-2a"
+    availability_zone = data.aws_availability_zones.available.names
     tags = {
         "Name" = "final-subnet-1"
     }
@@ -26,6 +29,6 @@ resource "aws_subnet" "final-subnet-2" {
     map_public_ip_on_launch = true
     availability_zone = "us-east-2b"
     tags = {
-        "Name" = "final-subnet-2"
+        "Name" = data.aws_availability_zones.available.names
     }
 }
